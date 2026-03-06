ec35b6576c761971146c29f02115ce957062ce4e
almartdev <alonsomartinezcarratala@gmail.com>
2026-03-06 23:52:14 +0100

    Added Keycodes from SDL2

--- engine/include/engine/input/keycodes.h
+++ engine/include/engine/input/keycodes.h
@@ -5,7 +5,55 @@
 #define KEY_A          4
 #define KEY_D          7
 #define KEY_E          8
+#define KEY_Q          20
+#define KEY_R          21
+#define KEY_F          9
+#define KEY_G          10
+#define KEY_Z          29
+#define KEY_X          27
+#define KEY_C          6
+#define KEY_V          25
+#define KEY_B          5
+
+#define KEY_1          30
+#define KEY_2          31
+#define KEY_3          32
+#define KEY_4          33
+#define KEY_5          34
+#define KEY_6          35
+#define KEY_7          36
+#define KEY_8          37
+#define KEY_9          38
+#define KEY_0          39
+
+#define KEY_ENTER      40
 #define KEY_SPACE      44
 #define KEY_ESCAPE     41
+#define KEY_TAB        43
+#define KEY_BACKSPACE  42
+#define KEY_SHIFT      225
+#define KEY_CTRL       224
+#define KEY_ALT        226
+#define KEY_CAPSLOCK   57
+
+#define KEY_UP         82
+#define KEY_DOWN       81
+#define KEY_LEFT       80
+#define KEY_RIGHT      79
+
+#define KEY_F1         58
+#define KEY_F2         59
+#define KEY_F3         60
+#define KEY_F4         61
+#define KEY_F5         62
+#define KEY_F6         63
+#define KEY_F7         64
+#define KEY_F8         65
+#define KEY_F9         66
+#define KEY_F10        67
+#define KEY_F11        68
+#define KEY_F12        69
+
 #define MOUSE_LEFT     1
 #define MOUSE_RIGHT    3
+#define MOUSE_MIDDLE   2
