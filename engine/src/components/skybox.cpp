f1df355b1b09ad03a23dea72544fb2e215026107
aloyak <almartdev@proton.me>
2026-03-12 16:16:10 +0100

    Fixed Skyboxes on Web Builds

--- engine/src/components/skybox.cpp
+++ engine/src/components/skybox.cpp
@@ -5,7 +5,12 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp> 
 
-#include <glad/glad.h>
+#ifdef __EMSCRIPTEN__
+    #include <GLES3/gl3.h>
+#else
+    #include <glad/glad.h>
+#endif
+
 #include <stb_image.h>
 #include <spdlog/spdlog.h>
 #include <nlohmann/json.hpp>
