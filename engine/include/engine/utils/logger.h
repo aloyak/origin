505f5c06135cdeaa27352a0efc64ff0e9cef8113
aloyak <almartdev@proton.me>
2026-06-09 13:18:58 +0200

    Add formating functionality to logging class

--- engine/include/engine/utils/logger.h
+++ engine/include/engine/utils/logger.h
@@ -2,8 +2,7 @@
 
 #include <string>
 #include <functional>
-
-#include <format> // C++20 formatting library, may cause issues, remove if so
+#include <format> // C++20
 
 class Logger {
 public:
@@ -11,13 +10,37 @@ public:
 
     static void setVerbose(int verbose);
     static void setCallback(Callback callback);
-    
-    static void log(const std::string& message); // Same as info
-    
+
+    static void log(const std::string& message);
     static void debug(const std::string& message);
     static void info(const std::string& message);
     static void warn(const std::string& message);
     static void error(const std::string& message);
+
+    template<typename... Args>
+    static void log(std::format_string<Args...> fmt, Args&&... args) {
+        log(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    static void debug(std::format_string<Args...> fmt, Args&&... args) {
+        debug(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    static void info(std::format_string<Args...> fmt, Args&&... args) {
+        info(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    static void warn(std::format_string<Args...> fmt, Args&&... args) {
+        warn(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    static void error(std::format_string<Args...> fmt, Args&&... args) {
+        error(std::format(fmt, std::forward<Args>(args)...));
+    }
 
     static void clear();
 };
