f183ff53652516051336c9cdce68f7cbde9605e4
aloyak <almartdev@proton.me>
2026-06-27 16:34:18 +0200

    add dynamic user settings system

---
+++ engine/include/engine/core/userSettings.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
+
+#include "engine/utils/path.h"
+
+template <typename T>
+class SettingsManager {
+private:
+    std::string filePath = "user/settings.json";
+    T currentSettings;
+
+public:
+    SettingsManager(const std::string& path) : filePath(path) {}
+
+    T& Get() {
+        return currentSettings;
+    }
+
+    void Save() {
+        nlohmann::json j = currentSettings;
+        
+        std::ofstream file(Path::resolve(filePath));
+        if (file.is_open()) {
+            file << j.dump(4);
+            file.close();
+        }
+    }
+
+    void Load() {
+        std::ifstream file(Path::resolve(filePath));
+        if (file.is_open()) {
+            nlohmann::json j;
+            file >> j;
+            currentSettings = j.get<T>();
+            file.close();
+        }
+    }
+};
