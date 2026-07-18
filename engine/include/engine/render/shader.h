191b33df631750b0e3d7986db94901ccddeec37a
aloyak <almartdev@proton.me>
2026-07-18 10:22:32 +0200

    fix random backslash in setMat4

--- engine/include/engine/render/shader.h
+++ engine/include/engine/render/shader.h
@@ -20,7 +20,7 @@ public:
     void setVec3(const std::string& name, const Vec3& value) const;
     void setVec4(const std::string& name, const Vec4& value) const;
 
-    void setMat4(const std::string& name, const Mat4& value) const;\
+    void setMat4(const std::string& name, const Mat4& value) const;
 
     void setTexture(const std::string& name, const class Texture& texture, int unit) const;
     
